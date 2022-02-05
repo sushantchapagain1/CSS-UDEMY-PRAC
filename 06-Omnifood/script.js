982fa8b1209c5481ae63246ae9f2ce11ebe604cc
sreddevil123123 <sreddevil2@gmail.com>
2022-02-05 15:33:45 +0545

    loader

--- 06-Omnifood/script.js
+++ 06-Omnifood/script.js
@@ -9,3 +9,37 @@ const header = document.querySelector('.header');
 menuBtn.addEventListener('click', () => {
   header.classList.toggle('open-nav');
 });
+
+// Smooth Scrolls with event delagation
+
+const allLinks = document.querySelectorAll('a:link');
+
+allLinks.forEach((links) => {
+  links.addEventListener('click', (e) => {
+    e.preventDefault();
+    const currentLink = links.getAttribute('href');
+    if (currentLink === '#')
+      window.scrollTo({
+        top: 0,
+        behavior: 'smooth',
+      });
+    if (currentLink !== '#' && currentLink.startsWith('#')) {
+      const sectionEL = document.querySelector(currentLink);
+      sectionEL.scrollIntoView({
+        behavior: 'smooth',
+      });
+    }
+    // Close nav on mobile after clicking links
+    if (header.classList.contains('open-nav')) {
+      header.classList.toggle('open-nav');
+    }
+  });
+});
+
+// Fixed Navbar
+
+// Loader
+const loader = document.querySelector('.loader');
+window.onload = function () {
+  loader.style.display = 'none';
+};
